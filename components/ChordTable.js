e43208b081a1d271d1364cc9802d0cf8fb9bdd52
bigo <bigodines@gmail.com>
2021-07-14 21:30:25 -0700

    create a theme component to wrap content across routes

--- components/ChordTable.js
+++ components/ChordTable.js
@@ -1,7 +1,23 @@
+import { TextField } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        '& > *': {
+            margin: theme.spacing(1),
+            width: '25ch'
+        }
+    }
+}))
+
 export default function ChordTable() {
+    const classes = useStyles()
+
     return (
-        <div className={styles.hello}>
-            <p>Hello World</p>
+        <div>
+            <form className={classes.root} noValidate autoComplete="off">
+                <TextField id="outlined-basic" label="Fundamental" variant="standard" />
+            </form>
         </div>
     )
 }
