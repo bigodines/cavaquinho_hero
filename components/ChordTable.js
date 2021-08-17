91550d5a8f138d2e183eff3b84638236270226fb
bigo <bigodines@gmail.com>
2021-08-16 22:44:35 -0700

    wip. another short session :(

--- components/ChordTable.js
+++ components/ChordTable.js
@@ -1,6 +1,7 @@
-import { Button, TextField } from '@material-ui/core'
+import { Table, TableBody, TableCell, TableRow, TableHead, Paper, TableContainer, Button, TextField } from '@material-ui/core'
+
 import { makeStyles } from '@material-ui/core/styles'
-import React from 'react'
+import React, { useState } from 'react'
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -14,18 +15,46 @@ const useStyles = makeStyles((theme) => ({
 
 export default function ChordTable() {
     const classes = useStyles()
-    const [note, setNote] = React.useState('C')
+    const [note, setNote] = useState('C')
+    const [showTable, setShowTable] = useState(false)
+
+    const chords = {}
 
     const handleNoteChange = (event) => {
-        console.log('worked')
+        setNote(event.target.value)
+        setShowTable(true)
     }
 
     return (
         <div className={classes.root}>
             <form noValidate autoComplete="off">
-                <TextField id="outlined-basic" label="Fundamental" variant="standard" value={note} />
-                <Button variant="contained" onClick={handleNoteChange}>OK</Button>
+                <TextField id="outlined-basic" label="Fundamental" variant="standard" value={note} onChange={handleNoteChange} />
+                <Button variant="contained">OK</Button>
             </form>
+            { showTable && <TableContainer component={Paper}>
+                <Table className={classes.table} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Acorde</TableCell>
+                            <TableCell align="right">Notas</TableCell>
+                            <TableCell align="right">Extras</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows.map((row) => (
+                            <TableRow key={row.name}>
+                                <TableCell component="th" scope="row">
+                                    {row.name}
+                                </TableCell>
+                                <TableCell align="right">{row.calories}</TableCell>
+                                <TableCell align="right">{row.fat}</TableCell>
+                                <TableCell align="right">{row.carbs}</TableCell>
+                                <TableCell align="right">{row.protein}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>}
         </div>
     )
 }
