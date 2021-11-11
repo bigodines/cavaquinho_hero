2caf5af7033765a68981f0246cb5bf97cc0db4bc
bigo <bigodines@gmail.com>
2021-11-10 21:16:07 -0800

    habemus campo harmonico maior

--- components/HarmonicField.js
+++ components/HarmonicField.js
@@ -1,4 +1,49 @@
+import { Table, TableBody, TableCell, TableRow, TableHead, Paper, TableContainer, Button, TextField } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import React, { useState } from 'react'
+
+import { MajorDiatonicScale } from '../lib/tonality'
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        '& > *': {
+            margin: theme.spacing(1),
+            width: '25ch',
+            flexgrow: 1
+        }
+    },
+    table: {
+        minWidth: 650,
+        width: 1200
+    },
+    noteForm: {
+        textAlign: 'center'
+    }
+}))
+
 export default function HarmonicField() {
+    const classes = useStyles()
+    const [note, setNote] = useState('')
+    const [showTable, setShowTable] = useState(false)
+    const [chords, setChords] = useState([])
+
+    const handleNoteChange = (event) => {
+        setNote(event.target.value)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+
+        const diatonicScale = MajorDiatonicScale(note)
+
+        if (diatonicScale?.length !== 7) {
+            console.error('invalid scale')
+        }
+        setChords(diatonicScale)
+        setShowTable(true)
+        console.log(chords)
+    }
+
     return (
         <div>
             <form noValidate autoComplete="off" onSubmit={handleSubmit} className={classes.noteForm}>
@@ -9,12 +54,23 @@ export default function HarmonicField() {
                 <Table className={classes.table} aria-label="simple table">
                     <TableHead>
                         <TableRow>
-                            <TableCell>Acorde</TableCell>
-                            <TableCell align="right">Notas</TableCell>
-                            <TableCell align="right">Extras</TableCell>
+                            <TableCell align="center">I</TableCell>
+                            <TableCell align="center">II</TableCell>
+                            <TableCell align="center">III</TableCell>
+                            <TableCell align="center">IV</TableCell>
+                            <TableCell align="center">V</TableCell>
+                            <TableCell align="center">VI</TableCell>
+                            <TableCell align="center">VII</TableCell>
                         </TableRow>
                     </TableHead>
-                    { renderChords() }
+                    <TableBody>
+                        <TableRow key="diatonic">
+                            { chords.map((v) => {
+                                return (<TableCell align="center" key={v}>{v}</TableCell>)
+                            })}
+
+                        </TableRow>
+                    </TableBody>
                 </Table>
             </TableContainer>}
         </div>
--- pages/chords.js
+++ pages/chords.js
@@ -1,8 +1,39 @@
+import Header from '../components/Header'
+import ChordTable from '../components/ChordTable'
+import React from 'react'
+import { Container, Grid, makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles((theme) => ({
+    content: {
+        flexGrow: 1,
+        height: '100vh',
+        overflow: 'auto'
+    },
+
+    container: {
+        paddingTop: theme.spacing(4),
+        paddingBottom: theme.spacing(4)
+    },
+
+    toolbar: theme.mixins.toolbar
+
+}))
 
 export default function Chords() {
+    const classes = useStyles()
+
     return (
         <div>
-            <p>Hello World</p>
+            <Header />
+            <Container maxWidth="lg" className={classes.container}>
+                <Grid container spacing={4}>
+                    <div className={classes.toolbar}>
+                        <Grid item xs={12}>
+                            <ChordTable />
+                        </Grid>
+                    </div>
+                </Grid>
+            </Container>
         </div>
     )
 }
