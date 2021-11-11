64b8c4c111adf931160cc54f2b222ee665005b35
bigo <bigodines@gmail.com>
2021-11-10 21:49:36 -0800

    major housekeeping madness

--- components/Header.js
+++ components/Header.js
@@ -7,160 +7,162 @@ import {
     IconButton,
     Drawer,
     Link,
-    MenuItem,
-  } from "@material-ui/core";
-import MenuIcon from "@material-ui/icons/Menu";
-import React, { useState, useEffect } from "react";
-import ActiveLink from "./ActiveLink";
-  
-  const headersData = [
+    MenuItem
+} from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu'
+import React, { useState, useEffect } from 'react'
+import ActiveLink from './ActiveLink'
+
+const headersData = [
     {
-      label: "Acordes",
-      href: "/chords",
+        label: 'Acordes',
+        href: '/chords'
     },
     {
-      label: "Campo Harmonico",
-      href: "/harmonic_field",
+        label: 'Campo Harmonico',
+        href: '/harmonic_field'
     },
     {
-      label: "Sobre",
-      href: "/about",
-    },
-  ];
-  
-  const useStyles = makeStyles(() => ({
+        label: 'Sobre',
+        href: '/about'
+    }
+]
+
+const Offset = styled("div")(({ theme }) => theme.mixins.toolbar);
+
+const useStyles = makeStyles(() => ({
     header: {
-      backgroundColor: "#400CCC",
-      paddingRight: "79px",
-      paddingLeft: "118px",
-      "@media (max-width: 900px)": {
-        paddingLeft: 0,
-      },
+        backgroundColor: '#400CCC',
+        paddingRight: '79px',
+        paddingLeft: '118px',
+        '@media (max-width: 900px)': {
+            paddingLeft: 10
+        }
     },
     logo: {
-      fontFamily: "Work Sans, sans-serif",
-      fontWeight: 600,
-      color: "#FFFEFE",
-      textAlign: "left",
+        fontFamily: 'Work Sans, sans-serif',
+        fontWeight: 600,
+        color: '#FFFEFE',
+        textAlign: 'left'
     },
     menuButton: {
-      fontFamily: "Open Sans, sans-serif",
-      fontWeight: 700,
-      size: "18px",
-      marginLeft: "38px",
+        fontFamily: 'Open Sans, sans-serif',
+        fontWeight: 700,
+        size: '18px',
+        marginLeft: '38px'
     },
     toolbar: {
-      display: "flex",
-      justifyContent: "space-between",
+        display: 'flex',
+        justifyContent: 'space-between'
     },
     drawerContainer: {
-      padding: "20px 30px",
-    },
-  }));
-  
-  export default function Header() {
-    const { header, logo, menuButton, toolbar, drawerContainer } = useStyles();
-  
+        padding: '20px 30px'
+    }
+}))
+
+export default function Header() {
+    const { header, logo, menuButton, toolbar, drawerContainer } = useStyles()
+
     const [state, setState] = useState({
-      mobileView: false,
-      drawerOpen: false,
-    });
-  
-    const { mobileView, drawerOpen } = state;
-  
+        mobileView: false,
+        drawerOpen: false
+    })
+
+    const { mobileView, drawerOpen } = state
+
     useEffect(() => {
-      const setResponsiveness = () => {
-        return window.innerWidth < 900
-          ? setState((prevState) => ({ ...prevState, mobileView: true }))
-          : setState((prevState) => ({ ...prevState, mobileView: false }));
-      };
-  
-      setResponsiveness();
-  
-      window.addEventListener("resize", () => setResponsiveness());
-  
-      return () => {
-        window.removeEventListener("resize", () => setResponsiveness());
-      };
-    }, []);
-  
+        const setResponsiveness = () => {
+            return window.innerWidth < 900
+                ? setState((prevState) => ({ ...prevState, mobileView: true }))
+                : setState((prevState) => ({ ...prevState, mobileView: false }))
+        }
+
+        setResponsiveness()
+
+        window.addEventListener('resize', () => setResponsiveness())
+
+        return () => {
+            window.removeEventListener('resize', () => setResponsiveness())
+        }
+    }, [])
+
     const displayDesktop = () => {
-      return (
-        <Toolbar className={toolbar}>
-          {chLogo}
-          <div>{getMenuButtons()}</div>
-        </Toolbar>
-      );
-    };
-  
+        return (
+            <Toolbar className={toolbar}>
+                {chLogo}
+                <div>{getMenuButtons()}</div>
+            </Toolbar>
+        )
+    }
+
     const displayMobile = () => {
-      const handleDrawerOpen = () =>
-        setState((prevState) => ({ ...prevState, drawerOpen: true }));
-      const handleDrawerClose = () =>
-        setState((prevState) => ({ ...prevState, drawerOpen: false }));
-  
-      return (
-        <Toolbar>
-          <IconButton
-            {...{
-              edge: "start",
-              color: "inherit",
-              "aria-label": "menu",
-              "aria-haspopup": "true",
-              onClick: handleDrawerOpen,
-            }}
-          >
-            <MenuIcon />
-          </IconButton>
-  
-          <Drawer
-            {...{
-              anchor: "left",
-              open: drawerOpen,
-              onClose: handleDrawerClose,
-            }}
-          >
-            <div className={drawerContainer}>{getDrawerChoices()}</div>
-          </Drawer>
-  
-          <div>{chLogo}</div>
-        </Toolbar>
-      );
-    };
-  
+        const handleDrawerOpen = () =>
+            setState((prevState) => ({ ...prevState, drawerOpen: true }))
+        const handleDrawerClose = () =>
+            setState((prevState) => ({ ...prevState, drawerOpen: false }))
+
+        return (
+            <Toolbar>
+                <IconButton
+                    {...{
+                        edge: 'start',
+                        color: 'inherit',
+                        'aria-label': 'menu',
+                        'aria-haspopup': 'true',
+                        onClick: handleDrawerOpen
+                    }}
+                >
+                    <MenuIcon />
+                </IconButton>
+
+                <Drawer
+                    {...{
+                        anchor: 'left',
+                        open: drawerOpen,
+                        onClose: handleDrawerClose
+                    }}
+                >
+                    <div className={drawerContainer}>{getDrawerChoices()}</div>
+                </Drawer>
+
+                <div>{chLogo}</div>
+            </Toolbar>
+        )
+    }
+
     const getDrawerChoices = () => {
-      return headersData.map(({ label, href }) => {
-        return (
-          <ActiveLink activeClassName="active" href={href} key={label}>
-            <MenuItem>{label}</MenuItem>
-          </ActiveLink>
-        );
-      });
-    };
-  
+        return headersData.map(({ label, href }) => {
+            return (
+                <ActiveLink activeClassName="active" href={href} key={label}>
+                    <MenuItem>{label}</MenuItem>
+                </ActiveLink>
+            )
+        })
+    }
+
     const chLogo = (
-      <Typography variant="h6" component="h1" className={logo}>
+        <Typography variant="h6" component="h1" className={logo}>
         Cavaquinho Hero
-      </Typography>
-    );
-  
+        </Typography>
+    )
+
     const getMenuButtons = () => {
-      return headersData.map(({ label, href }) => {
-        return (
-          <ActiveLink href={href}
-              key={label}
-              activeClassName="active"
-              className={menuButton}>
-            <dv>{label}</dv>
-          </ActiveLink>
-        );
-      });
-    };
-  
+        return headersData.map(({ label, href }) => {
+            return (
+                <ActiveLink href={href}
+                    key={label}
+                    activeClassName="active"
+                    className={menuButton}>
+                    <dv>{label}</dv>
+                </ActiveLink>
+            )
+        })
+    }
+
     return (
         <AppBar className={header} position="sticky">
-          {mobileView ? displayMobile() : displayDesktop()}
+            {mobileView ? displayMobile() : displayDesktop()}
         </AppBar>
-    );
-  }
-  
+        <Offset />
+    )
+}
